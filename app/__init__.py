9ed97031916b8a9f575fa3bd1ae1fda79b35fb31
Brian Otieno <bryomajor@gmail.com>
2019-10-28 09:38:54 +0300

    Rectify typo in the auth blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -41,6 +41,6 @@ def create_app(config_name):
 
     # Registering the auth blueprint
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
