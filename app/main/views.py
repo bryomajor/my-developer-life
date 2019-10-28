be163b343b167f37ced8abc607324d302876876d
Brian Otieno <bryomajor@gmail.com>
2019-10-28 09:12:03 +0300

    Add view functions that return the profile

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,11 @@ from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
 from flask_login import login_required, current_user
 from ..models import BlogPost, User, Comment
-from .forms import BlogPostForm, CommentForm
+from .forms import BlogPostForm, CommentForm, UpdateProfile
 from flask.views import View, MethodView
-from .. import db
+from .. import db, photos
 
-@main.route('/')
+@main.route('/', methods = ['GET', 'POST'])
 def index():
     '''
     View function that returns the index page and it's data
@@ -65,3 +65,43 @@ def contact():
     View function that returns the contact page
     '''
     return render_template('contact.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html', user = user)
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form = form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
